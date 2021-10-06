1da8ebf1484c0ba487a2860a7c1901bdb2ab50ee
HEINRICK7 <heinrickcostta@gmail.com>
2021-10-06 08:18:46 -0300

    create src/index and src/database

---
+++ src/database/index.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/noderest', {
+    useMongoClient: true
+});
+mongoose.Promise = global.Promise;
+
+module.exports = mongoose;
