dd99c2127b23179c1191d2ce62d06ca151d542f5
HEINRICK7 <heinrickcostta@gmail.com>
2021-10-08 10:14:38 -0300

    create src/models/Travel.js

---
+++ src/models/Travel.js
@@ -0,0 +1,52 @@
+const mongoose = require('../database');
+
+const TravelSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        require: true,
+    },
+    date: {
+        type: Date,
+        require: true
+    },
+    departure_time: {
+        type: String,
+        require: true
+    },
+    price: {
+        type: String,
+        require: true
+    },
+    quant_min: {
+        type: Number,
+        require: true
+    },
+    quant_max: {
+        type: Number,
+        require: true
+    },
+    quant_day: {
+        type: Number,
+        require: true
+    },
+    description: {
+        type: String,
+        require: true,
+    },
+    image_url: {
+        type: String,
+        require: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    admin_id:[{ 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref:'Admin',
+        require: true
+    }]
+});
+const Travel = mongoose.model('Travel', TravelSchema);
+
+module.exports = Travel;
