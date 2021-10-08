5f7ed8843d6289e0e6f266576544ab0985adb731
HEINRICK7 <heinrickcostta@gmail.com>
2021-10-08 10:10:19 -0300

    modifield packages

--- src/database/index.js
+++ src/database/index.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/noderest', {
-    useMongoClient: true
-});
+const mongoURI = "mongodb+srv://travel:travel@cluster0.bnncd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+mongoose.connect( mongoURI, 
+{   
+    useUnifiedTopology: true, 
+    useNewUrlParser: true, 
+    useNewUrlParser: true }) 
+.then(() => console.log("MongoDB connected")) 
+.catch((err) => console.log(err));
+
+
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
