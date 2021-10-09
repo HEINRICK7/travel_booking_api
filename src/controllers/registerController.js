5d2de1db36e93f98a6362ca36cc85ae5bc2de47b
HEINRICK7 <heinrickcostta@gmail.com>
2021-10-09 07:11:51 -0300

    create registerController

---
+++ src/controllers/registerController.js
@@ -0,0 +1,25 @@
+
+const Admin = require('../models/Admin');
+
+module.exports = {
+
+async store(req, res){
+    const { email } = req.body;
+
+    try {
+        if(await Admin.findOne({email}))
+            return res.status(400).send({error: 'Admin already exists'})
+
+            const admin = await Admin.create(req.body);
+
+            admin.password = undefined;
+
+
+            return res.send({ admin });
+
+    }
+    catch (err){
+        return res.status(400).send({ error: 'Registration failed'});
+    }
+}
+}
