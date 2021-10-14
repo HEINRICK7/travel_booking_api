6eeb10d0cf1d00903ce8974f23e63eb10858b94c
HEINRICK7 <heinrickcostta@gmail.com>
2021-10-14 15:18:13 -0300

    modifield files

--- src/controllers/travelUserController.js
+++ src/controllers/travelUserController.js
@@ -1,23 +1,27 @@
 
 const TravelUser = require('../models/TravelUser');
 const TravelAdmin = require('../models/Travel');
+
 module.exports = {
 
 async store(req, res){
     const { travel_id } = req.params;
     const { cpf } = req.body;
 
+    const travel_id = await TravelAdmin.findById({travel_id})
+    const user = await TravelUser.findByOne({cpf})
     try {
-        if(await TravelUser.findOne({cpf}))
-            return res.status(400).send({error: 'Admin already exists'})
-
+        if(user && travel_id){
+            return res.status(400).send({error: 'Usuario e viagem ja cadastrados'})
+        }
+        else {
             const travelUser = await TravelUser.create(req.body,travel_id);
 
             return res.send({ 
                 travelUser,
                 travel_id
             });
-
+        }
     }
     catch (err){
         return res.status(400).send({ error: 'Registration failed'});
