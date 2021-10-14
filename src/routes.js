b0344ce87b1d7b15847af0f2a93473adf5e8f3ac
HEINRICK7 <heinrickcostta@gmail.com>
2021-10-14 09:56:20 -0300

    modifield routes.js

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ const authMiddleware = require('./middlewares/auth')
 const LoginController = require('./controllers/LoginController');
 const RegisterController = require('./controllers/registerController');
 const TravelController = require('./controllers/travelController');
+const TravelUserController = require('./controllers/travelUserController')
 
 
 routes.post('/login', LoginController.store);
@@ -14,6 +15,9 @@ routes.post('/register',authMiddleware, RegisterController.store);
 
 
 routes.post('/travel_register',authMiddleware, TravelController.store);
+
+routes.post('/travel_register_user', TravelUserController.store);
+
 routes.get('/travel_all', TravelController.index);
 routes.get('/travel/:_id', TravelController.show);
 //routes.delete('/results/:id', ResultController.destroy);
