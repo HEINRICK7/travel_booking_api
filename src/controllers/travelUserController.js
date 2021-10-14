0ebb717cda78b32be47c1aa2858c62d2fec5c134
HEINRICK7 <heinrickcostta@gmail.com>
2021-10-14 15:24:44 -0300

    modifield files

--- src/controllers/travelUserController.js
+++ src/controllers/travelUserController.js
@@ -8,10 +8,10 @@ async store(req, res){
     const { travel_id } = req.params;
     const { cpf } = req.body;
 
-    const travel_id = await TravelAdmin.findById({travel_id})
+    const travelId = await TravelAdmin.findById({travel_id})
     const user = await TravelUser.findByOne({cpf})
     try {
-        if(user && travel_id){
+        if(user && travelId){
             return res.status(400).send({error: 'Usuario e viagem ja cadastrados'})
         }
         else {
