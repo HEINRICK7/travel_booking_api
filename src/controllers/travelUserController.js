0092606a7e2177774d63bc1af683f5e3102e0ac0
HEINRICK7 <heinrickcostta@gmail.com>
2021-10-14 18:29:50 -0300

    modifield files

--- src/controllers/travelUserController.js
+++ src/controllers/travelUserController.js
@@ -1,26 +1,23 @@
 
 const TravelUser = require('../models/TravelUser');
 const TravelAdmin = require('../models/Travel');
-
 module.exports = {
 
 async store(req, res){
     const { travel_id } = req.params;
     const { cpf } = req.body;
 
-    const travelId = await TravelAdmin.findById({travel_id})
-    const user = await TravelUser.findByOne({cpf})
     try {
-        if(user && travelId){
-            return res.status(400).send({error: 'Usuario e viagem ja cadastrados'})
-        }
-        else {
+        if(!await TravelUser.findOne({cpf})){
             const travelUser = await TravelUser.create(req.body,travel_id);
 
-            return res.send({ 
-                travelUser,
-                travel_id
-            });
+                return res.send({ 
+                    travelUser,
+                    travel_id
+                });
+        }else{
+            
+            return res.status(400).send({error: 'Admin already exists'})
         }
     }
     catch (err){
--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ require('./database/index');
 
 const app = express();
 
-app.use(cors())
+app.use(cors('*'))
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(router);
