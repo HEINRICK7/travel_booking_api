9c7ef304ad86a3d56f21ed8db7dc216c57436d0c
Carlos Henrique da Costa <heinrickcostta@gmail.com>
2021-10-24 13:48:05 -0300

    update

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ require('./database/index');
 
 const app = express();
 
-app.use(cors('*'))
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
