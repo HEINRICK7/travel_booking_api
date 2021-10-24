83dc969fd81300800a7e59a165eee47dc22645ed
Carlos Henrique da Costa <heinrickcostta@gmail.com>
2021-10-24 15:20:14 -0300

    update

--- src/server.js
+++ src/server.js
@@ -26,11 +26,4 @@ app.use(
   
 const port = process.env.PORT || 3333;
 
-app.listen( port, (err)=>{
-    if(err){
-        console.log(err);
-    }else{
-         console.log(`connected server http://localhost:${port}`);
-    }     
-})
 
