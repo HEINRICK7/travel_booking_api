8b8f2383f67dd8d13baa8a43e2a0cd6dc4f689d3
Carlos Henrique da Costa <heinrickcostta@gmail.com>
2021-10-24 11:42:19 -0300

    create src/config/multer

---
+++ src/config/multer.js
@@ -0,0 +1,64 @@
+const multer = require("multer");
+const path = require("path");
+const crypto = require("crypto");
+const S3 = require("aws-sdk/clients/s3");
+const multerS3 = require("multer-s3");
+
+const MAX_SIZE_TWO_MEGABYTES = 2 * 1024 * 1024;
+
+const s3 = new S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION,
+    })
+
+const storageTypes = {
+  local: multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, path.resolve(__dirname, "..", "..", "tmp", "uploads"));
+    },
+    filename: (req, file, cb) => {
+      crypto.randomBytes(16, (err, hash) => {
+        if (err) cb(err);
+      
+        file.key = `${hash.toString("hex")}-${file.originalname}`;
+
+        cb(null, file.key);
+      });
+    },
+  }),
+  s3: multerS3({
+    s3: s3,
+    bucket: process.env.BUCKET_NAME,
+    acl: "public-read",
+    key: (req, file, cb) => {
+      crypto.randomBytes(16, (err, hash) => {
+        if (err) cb(err);
+        const fileName = `${hash.toString("hex")}-${file.originalname}`;
+        cb(null, fileName);
+      });
+    },
+  }),
+};
+
+module.exports = {
+  dest: path.resolve(__dirname, "..", "..", "tmp", "uploads"),
+  storage: storageTypes[process.env.STORAGE_TYPE],
+  limits: {
+    fileSize: MAX_SIZE_TWO_MEGABYTES,
+  },
+  fileFilter: (req, file, cb) => {
+    const allowedMimes = [
+      "image/jpeg",
+      "image/pjpeg",
+      "image/png",
+      "image/gif",
+    ];
+
+    if (allowedMimes.includes(file.mimetype)) {
+      cb(null, true);
+    } else {
+      cb(new Error("Invalid file type."));
+    }
+  },
+};
